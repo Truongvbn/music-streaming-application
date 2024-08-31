f7befca33d8eecf7b308b525c16840a82310a303
Truongvbn <vobanhattruong2@gmail.com>
2024-08-31 14:55:58 +0700

    test

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from '../layout/Rootlayout';
 import Home from '../pages/Home/Home';
 import Login from '../pages/auth/Login';
+import SignUp from '../pages/auth/SignUp';
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,7 @@ export const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: 'login', element: <Login /> },
+      { path: 'signup', element: <SignUp /> },
     ],
   },
 ]);
